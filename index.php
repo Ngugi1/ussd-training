e7bb1bda431bcf10d498f8b4ec251ace4dd27539
Le-Yo <lenykoskey@yahoo.com>
2015-07-16 18:22:29 +0300

    added a few lines to index.php

--- index.php
+++ index.php
@@ -1,5 +1,20 @@
 <?php
 
+$sessionId   = $_REQUEST["sessionId"];
+$serviceCode = $_REQUEST["serviceCode"];
+$phoneNumber = $_REQUEST["phoneNumber"];
+$text        = $_REQUEST["text"];
+
+
+
+echo "END Your Phone number is: ".$phoneNumber.". Session is: ".$sessionId." You have sent text as: ".$text;
+//
+// *222*4*5*7#
+// Structure of a USSD Code
+// *222*Leo*Secondname*34234*Nairobi#
+
+
+
 // return "hello world";
 $leo = ['name'=>'Leo', 'staff_id' => 1234];
 
