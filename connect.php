1b2dbd1f63cc161fea79085cdebef3a3aa07a5f1
Le-Yo <lenykoskey@yahoo.com>
2015-07-20 18:23:04 +0300

    enabled error reporting

--- connect.php
+++ connect.php
@@ -1,12 +1,15 @@
 <?php
+ini_set('display_startup_errors',1);
+ini_set('display_errors',1);
+error_reporting(-1);
 
 $hostname = "localhost";
-$username = "username";//root
+$username = "leo";//root
 $password = "password";//""
 $connection =  mysql_connect($hostname, $username, $password) or
  trigger_error(mysql_error(),E_USER_ERROR);
 
-mysql_select_db("database");
+mysql_select_db("kplc");
 
-
+print_r(mysql_error());
 ?>
